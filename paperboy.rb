12eabd53a7aac67aef190c4bc1bf217906de6767
Christian <chrizx0@gmail.com>
2016-06-10 10:15:14 -0400

    realized i needed @ symbols

--- paperboy.rb
+++ paperboy.rb
@@ -1,57 +1,59 @@
 #starting paperboy now,
 #lets make this paperboy class
 
-  class paperboy
+class Paperboy
     #he needs a few attributes, and apparently we
     #need his earnings to be read only.
-    attr_accessor :name, :experience, :side, :quota
-    attr_reader :earnings
-#ok so apparently every paperboy has a quota of *atleast 50
-#so im going to put that into my accessor,
-#also i dont think i need to have earnings in both the
-#accessor and reader so im going to delete it from the
-#accessor.
+  attr_accessor :name, :experience, :side, :quota
+  attr_reader :earnings
+  #ok so apparently every paperboy has a quota of *atleast 50
+  #so im going to put that into my accessor,
+  #also i dont think i need to have earnings in both the
+  #accessor and reader so im going to delete it from the
+  #accessor.
 
-#ok lets make an initializer then we will worry about
-# getting the other methods set up etc..
-def initialize
-  @name = name
-  @experience = experience
-  @side = side
-  @quota = quota
-  @earnings = earnings
-end
+  #ok lets make an initializer then we will worry about
+  # getting the other methods set up etc..
+  def initialize(name, quota = 50)
+    @name = name
+    @experience = 0
+    # @side = side
+    @quota = quota
+    @earnings = 0
+    @delivered = 0
+    @cash = 0
+  end
 
   #paperboy quota just realized that is a formula.
   #i think (experience / 2) + 50 seems about right, i almost just set it to 50 lol.
   def paperboy_quota_method
-    quota = (@experience /2) + 50
+    @quota = (@experience /2) + 50
   end
 
   # time for the next method, deliver. harder than i thought.
   def deliver(start_address, end_address)
 
-    delivered = end_address - start_address
-    puts  "you delivered #{delivered} papers today"
-    if delivered >= quota
-      do
-        cash = (delivered * 0.25) + ((delivered - quota)*0.50
-      else
-         cash = ((delivered * 0.25) - 2)
-       end
-       puts "you made #{cash} dollars for today."
-         experience = quota + (delivered/2)
-         puts "your current experience is #{experience}"
-       end
+    @delivered = end_address - start_address
+    puts  "you delivered #{@delivered} papers today"
+    if @delivered >= @quota
+      @cash = (@delivered * 0.25) + ((@delivered - @quota)*0.50)
+    else
+       @cash = ((@delivered * 0.25) - 2)
+    end
+    puts "you made #{@cash} dollars for today."
+    @experience += @delivered
+    puts "your current experience is #{@experience}"
+  end
 
-def report
-  puts "Report for: #{@name}, #{@name} has #{@experience} and delivered #{delivered} papers today
-  making #{cash} dollars for the day."
+  def report
+    puts "Report for: #{@name}, #{@name} has #{@experience} and delivered #{@delivered} papers today
+    making #{@cash} dollars for the day."
 
-end
+  end
 #i am very confused at the moment. will resolve soon.
-      end
-jason = Paperboy.new ("Jason"), #need to set up a paperboy, got confused brain system shutdown.)
+end
+
+jason = Paperboy.new ("Jason") #need to set up a paperboy, got confused brain system shutdown.)
 
 #will set up interactive system for paperboys to check daily earnings and new xp.
 #  def deliver(start_address, end_address)
