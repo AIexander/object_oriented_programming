09e315cb41a1c4ea4c222a55ff3ff00fc39b9b58
Christian <chrizx0@gmail.com>
2016-06-10 12:00:44 -0400

    realized i had my attr accessor in the wrong spot.

--- people.rb
+++ people.rb
@@ -1,11 +1,11 @@
 class Person
+  attr_accessor :name
   def initialize(name)
     @name = name
     puts "Hello my name is #{@name},"
 end
 
 class Student < Person
-attr_accessor :learn, :name
 
 def learn
   puts "I get it!!!"
@@ -16,7 +16,6 @@ class Instructor < Person
   attr_accessor :teach, :name
   def teach
     puts "Everything in Rooby is an 0bject!"
-
 end
 chris = Instructor.new("Chris")
 christina = Student.new("Christina")
