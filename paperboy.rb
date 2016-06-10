6ff69bcfcb270ea14bc54bee6d8128c33e7ea396
Christian <chrizx0@gmail.com>
2016-06-10 07:32:11 -0400

    replaced :earnings with :quota

--- paperboy.rb
+++ paperboy.rb
@@ -4,5 +4,10 @@
   class paperboy
     #he needs a few attributes, and apparently we
     #need his earnings to be read only.
-    attr_accessor :name, :experience, :side, :earnings
+    attr_accessor :name, :experience, :side, :quota
     attr_reader :earnings
+#ok so apparently every paperboy has a quota of 50
+#so im going to put that into my accessor,
+#also i dont think i need to have earnings in both the
+#accessor and reader so im going to delete it from the
+#accessor.
