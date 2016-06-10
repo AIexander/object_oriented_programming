aa4743cb6d2e25a6bbec4eff977572abf1b5a05b
Christian <chrizx0@gmail.com>
2016-06-10 07:28:00 -0400

    paperboy class

--- paperboy.rb
+++ paperboy.rb
@@ -1,1 +1,8 @@
 #starting paperboy now,
+#lets make this paperboy class
+
+  class paperboy
+    #he needs a few attributes, and apparently we
+    #need his earnings to be read only.
+    attr_accessor :name, :experience, :side, :earnings
+    attr_reader :earnings
