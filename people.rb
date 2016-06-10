769f871727b5aebfc27449aa62b2a6acccfe5c7d
Christian <chrizx0@gmail.com>
2016-06-10 12:09:50 -0400

    added teach/learn rquests at end.

--- people.rb
+++ people.rb
@@ -3,6 +3,7 @@ class Person
   def initialize(name)
     @name = name
     puts "Hello my name is #{@name},"
+  end
 end
 
 class Student < Person
@@ -11,10 +12,14 @@ def learn
   puts "I get it!!!"
 
 end
+end
 
 class Instructor < Person
   def teach
     puts "Everything in Rooby is an 0bject!"
+  end
 end
 chris = Instructor.new("Chris")
 christina = Student.new("Christina")
+chris.teach
+christina.learn
